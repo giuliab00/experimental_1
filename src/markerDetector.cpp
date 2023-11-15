64f77b47a96bff839497139b60b04440ce056be3
Marco Tabita <81355218+Tabi43@users.noreply.github.com>
2023-11-15 18:50:00 +0100

    Update markerDetector.cpp
    Dimenticato dualmente delle robe anche qua

--- src/markerDetector.cpp
+++ src/markerDetector.cpp
@@ -1,58 +1,57 @@
-#include <iostream>
 #include "aruco/aruco.h"
 #include "aruco/cvdrawingutils.h"
-
 #include "ros/ros.h"
 #include "image_transport/image_transport.h"
 #include "cv_bridge/cv_bridge.h"
 #include "sensor_msgs/image_encodings.h"
-
 #include "std_msgs/Int32.h"
+#include "std_msgs/Float32.h"
 #include "experimental_1/markerDistance.h"
+#include "opencv2/core/mat.hpp"
+#include "nav_msgs/Odometry.h"
 #include <cmath>
+#include <sstream>
+#include <iostream>
 #include <opencv2/highgui.hpp>
 #include <aruco_ros/aruco_ros_utils.h>
 
-#include "opencv2/core/mat.hpp"
-
-#include <sstream>
-
 /*  subscriers:
- * 		-/camera/color/image_raw
- * 		-/camera/depth/image_raw
- * 		-/requestMarkerId
+ * 		- /camera/color/image_raw		: image from the camera
+ *		- /camera/color/camera_info		: camera info for parameters
+ *		- /requestMarkerId				: actual marker id desired
  * 
  * 	publishers:
- * 		-/markerDistance
- * 
- * 		
+ * 		- /debug/image_raw				: debug publisher
+ * 		- /markerDistance				: marker data
  */
 
 class MarkerDetector {
 	
 	//Attributes
 	private:
+		/*------------Aruco------------*/		
 		//aruco marker detector used to recognize the markers
 		aruco::MarkerDetector mDetector_;
 		//vector where all recognized markers are save
 		std::vector<aruco::Marker> markers_;
 		//aruco camera parameter neeeded to identify marker position
-		aruco::CameraParameters camParam_;
+		aruco::CameraParameters camParam_;		
 		bool useRectifiedImages = true;
+		
 		//marker size
 		double marker_real_size_ = 0.2;
 		double marker_size_ = marker_real_size_;
-		// ?
-		bool useCamInfo_ ;
-		bool isCameraCompressed;
-		//CV image
+		/*-----------------------------*/
+				
+		//CV image decompressed from it_
 		cv::Mat inImage_;
+		
 		//id of the marker to find
 		int actual_marker_id_;
 		bool POV_window_b_;
+		
 		//message to publish
 		experimental_1::markerDistance toSend;
-
 		sensor_msgs::ImagePtr outputMsg;
 		
 		//ROS pubs/subs
@@ -67,47 +66,46 @@ class MarkerDetector {
 		//publisher of markerDistance
 		ros::Publisher markerPoseGoal_pub_;
 		//subscriber to markerId to find
-		ros::Subscriber requestMarkerId_sub_;
-		
-		
+		ros::Subscriber requestMarkerId_sub_;		
 		
 	public:
-		MarkerDetector() :
-			nh_("~"), it_(nh_), useCamInfo_(true) 
-		{
+		MarkerDetector() : nh_("~"), it_(nh_) {
+
 			//create the subscribe to camera image /camera/rgb/image_raw/compressed
-			image_sub_ = it_.subscribe("/camera/rgb/image_raw",1, &MarkerDetector::image_callback, this);
+			image_sub_ = it_.subscribe("/camera/color/image_raw",1, &MarkerDetector::image_callback, this);
 
 			//create subscriber to camera info in order to set the proper Aruco Camera Parameter
-			cam_info_sub = nh_.subscribe("/camera/rgb/camera_info", 1, &MarkerDetector::cam_info_callback, this);
+			cam_info_sub = nh_.subscribe("/camera/color/camera_info", 1, &MarkerDetector::cam_info_callback, this);
 
 			//Pub debug
-			debug_pub_ = it_.advertise("/debug/image_raw", 1);
-						
-			nh_.param<bool>("use_camera_info", useCamInfo_, false);
-			nh_.param<bool>("pov_window", POV_window_b_, true);
-			nh_.param<bool>("campressed_camera", isCameraCompressed, false); //To handle the compression
-			
-			camParam_ = aruco::CameraParameters();
+			debug_pub_ = it_.advertise("/debug/image_raw", 1);	
 			
 			//publisher to notify NavLogic about the distance from the marker
 			markerPoseGoal_pub_ = nh_.advertise<experimental_1::markerDistance>("/markerDistance",10);
+			
 			//subscriber to know the marker to found
 			requestMarkerId_sub_ = nh_.subscribe("/requestMarkerId",1, &MarkerDetector::find_marker_callback, this);
+
+			//Read params	
+			nh_.param<bool>("pov_window", POV_window_b_, false);
 			
-			//Create window for RosBot POV
-			cv::namedWindow("ROSBot POV", cv::WINDOW_AUTOSIZE);	
+			camParam_ = aruco::CameraParameters();
 			
+			//Debug window for local running
+			if(POV_window_b_){
+				//Create window for RosBot POV
+				cv::namedWindow("ROSBot POV", cv::WINDOW_AUTOSIZE);	
+			}	
 		}
 
+		/*
+			Callback function of /camera/color/image_raw
+			each time an image is published update the list of marker detected
+			If the marker to found is detected compute it's distance from the center of the camera, to notify
+			if there's a misalignemet, and how many pixel is the side of the marker to see if the rosbot is still distant 
+	 	*/
 		void image_callback(const sensor_msgs::ImageConstPtr& msg) {
-			/*Callback function of /camera/color/image_raw
-   			each time an image is published update the list of marker detected
-      			If the marker to found is detected compute it's distance from the center of the camera, to notify
-	 		if there's a misalignemet, and how many pixel is the side of the marker to see if the rosbot is still distant 
-	 		*/
 			ros::Time curr_stamp = msg->header.stamp;
-			//create cv_bridge
 			cv_bridge::CvImagePtr cv_image_ptr; 
     
 			try {
@@ -118,6 +116,8 @@ class MarkerDetector {
 				//get center of the camera Frame
 				int width = static_cast<float>(inImage_.cols);
 				int height = static_cast<float>(inImage_.rows);
+
+				//Define the center of the camera image
 				cv::Point2f camera_center(width/2.0f, height/2.0f);
    
 				// clear out previous detection results
@@ -125,7 +125,6 @@ class MarkerDetector {
 
 				// detect marker
 				mDetector_.detect(inImage_, markers_, camParam_, marker_size_, false);
-
 				
 				for (std::size_t i = 0; i < markers_.size(); ++i) {
 					// draw detected markers on the image for visualization
@@ -146,16 +145,15 @@ class MarkerDetector {
 						
 						//set msg field
 						toSend.ack = 1; 
-						toSend.marker_id = markers_[i].id;
 						toSend.l_pixel = marker_distance;
 						toSend.centerDistance = center_distance;
-						//publish 
+						toSend.marker_id = markers_[i].id;
+						
+						//publish marker information
 						markerPoseGoal_pub_.publish(toSend);
 					}	
 				}
 
-
-				
 				//Visualize the camera POV
 				if(POV_window_b_){
 					cv::imshow("ROSBot POV", inImage_);
@@ -165,28 +163,30 @@ class MarkerDetector {
 				outputMsg = cv_bridge::CvImage(std_msgs::Header(), sensor_msgs::image_encodings::BGR8, inImage_).toImageMsg();
 				debug_pub_.publish(outputMsg);				
 			}
-				
-			catch (cv_bridge::Exception& e)
-			{
-			  ROS_ERROR("cv_bridge exception: %s", e.what());
+			
+			catch (cv_bridge::Exception& e) {
+				ROS_ERROR("cv_bridge exception: %s", e.what());
 			}
 		}
-			
+
+		/*
+			Callback function to retrive the desired marker id
+		*/
 		void find_marker_callback(const std_msgs::Int32 &msg) {
 			/*Update the marker to found once a new one has been published*/
 			actual_marker_id_ = msg.data;
-			ROS_INFO("Belin mi è arrivata una richiesta marker %d", msg.data);
-				
+			ROS_INFO("Belin mi e' arrivata una richiesta marker %d", msg.data);	
 		}
 		  
-		 // wait for one camerainfo to get Aruco CameraParam, then shut down that subscriber
-		 void cam_info_callback(const sensor_msgs::CameraInfo &msg) {
+		/*
+			wait for one camerainfo to get Aruco CameraParam, then shut down that subscriber
+		*/ 
+		void cam_info_callback(const sensor_msgs::CameraInfo &msg) {
 			/*Get one camera info to properly set the ArucoCameraParameter*/ 
 			camParam_ = aruco_ros::rosCameraInfo2ArucoCamParams(msg, useRectifiedImages);
 			//kill the subscriber
 			cam_info_sub.shutdown();
-			
-		  }
+		}
 				
 };
 
